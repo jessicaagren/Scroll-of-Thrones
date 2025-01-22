96c920eebdd4c3dd3cf86fa02921a587069228e7
jessicaagren <jessicaagren@hotmail.com>
2025-01-22 14:01:28 +0100

    Embossed box-shadow inlagd, ikoner inlagda

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import './style.scss'
+import './style.scss';
+
+import Quote from './types/quoteAPI';
 
 const main = document.querySelector("main") as HTMLElement;
 
 const mapIcon = document.getElementById("map-icon") as HTMLImageElement;
+const scrollIcon = document.getElementById("scroll-icon") as HTMLImageElement;
 
 mapIcon.addEventListener("click", addMap);
+scrollIcon.addEventListener("click", addScroll);
 
-// TODO lägg till DOM för karta
+// TODO lägg till DOM för karta + scroll
