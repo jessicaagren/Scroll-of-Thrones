3fb0731c54ffb37d73e6df9e8de0f9455ac23a92
jessicaagren <jessicaagren@hotmail.com>
2025-01-22 17:23:43 +0100

    Flyttat runt ikoner på boken

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,19 @@ mapIcon.addEventListener("click", addMap);
 scrollIcon.addEventListener("click", addScroll);
 
 // TODO lägg till DOM för karta + scroll
+
+// TODO testa lägga in karaktärer med DOM
+
+// TODO Läsa igenom Niklas repo!!!!!
+
+// TODO Lägg in sökfält någonstans i designen
+
+// TODO Lägg till dropshadow på bok och papper
+
+// TODO svh lvh och dvh
+
+// TODO lägg in husen med bild och koppla till API
+
+// TODO Sortera media
+
+// TODO lägg till overflow
