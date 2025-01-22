79a61a1f8c1148468985621eb40b5c6285e2029d
jessicaagren <jessicaagren@hotmail.com>
2025-01-22 12:01:23 +0100

    Påbörjat DOM-manipulation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import './style.scss'
+
+const main = document.querySelector("main") as HTMLElement;
+
+const mapIcon = document.getElementById("map-icon") as HTMLImageElement;
+
+mapIcon.addEventListener("click", addMap);
+
+// TODO lägg till DOM för karta
