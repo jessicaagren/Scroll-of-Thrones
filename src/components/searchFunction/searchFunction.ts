f514c1187adda91d3c456ceb5a10d1b489eb66f5
jessicaagren <jessicaagren@hotmail.com>
2025-01-29 13:52:31 +0100

    Lagt in placeholder för att kunna lägga in scss

--- src/components/searchFunction/searchFunction.ts
+++
@@ -1,9 +0,0 @@
-import { searchCharacters } from "../../api/asoiafAPI";
-import { article } from "../../constants/constants";
-
-const input = document.createElement("input");
-input.type = "text";
-input.placeholder = ". . .";
-input.id = "search-input";
-article.appendChild(input);
-
