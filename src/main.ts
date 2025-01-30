d5a133c12034b7032949dda1ece6af53506f34ac
jessicaagren <jessicaagren@hotmail.com>
2025-01-30 10:42:36 +0100

    Uppdaterat todo

--- src/main.ts
+++ src/main.ts
@@ -5,31 +5,13 @@ import { handleSearchClick } from './components/buttons/searchButton/searchButto
 import { handleSoundClick } from './components/buttons/soundButton/soundButton';
 import './style.scss';
 
-// TODO testa lägga in karaktärer med DOM
-
 // TODO (Läsa igenom Niklas repo)
-
-// TODO Gör så att sökfält uppdaterar med karaktärer
-
-// TODO Bara namn i mitten och klicka på namnet för att se info om karaktären till vänster
 
 // TODO Fixa responsivitet
 
 // TODO Göra alla knappar till en funktion?
 
-// TODO svh lvh och dvh?
-
 // TODO lägg in husen med bild och koppla till API
-
-// TODO lägg till overflow?
-
-// TODO Lägg upp quotes när kartan visas, med knapp för nytt random quote
-
-// TODO Animera placeholder i input?
-
-// TODO Gör article + aside till funktion för DRY?
-
-// TODO Fixa input så det ser ut som att den "står på pappret", t.ex. med blend mode för bakgrund och samma typsnitt som texten (kanske bara en underline utan border?)
 
 // TODO När man trycker på hus visas "Guess the house" och Begin. Visa upp en karaktär och fyra alternativ på hus. Fortsätt tills man svarar fel, spara poäng. Varje karaktär kan bara komma en gång. Alternativ på karaktär väljs från ASOIAF-API.
 
