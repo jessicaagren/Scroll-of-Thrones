7ee25999f20ee93c2fbc2da8ed2e11cc75e1a6d0
jessicaagren <jessicaagren@hotmail.com>
2025-01-30 16:26:02 +0100

    Uppdaterat funktion för DRY

--- src/components/buttons/houseGameButton/houseGameButton.ts
+++ src/components/buttons/houseGameButton/houseGameButton.ts
@@ -1,31 +1,7 @@
-import { article, drawingSwordAudio } from "../../../constants/constants";
-import { clearArticleAndAddBackground, clearAside, clearAndAddClickedClass, playSound } from "../../../helpers/helpers";
+import { drawingSwordAudio } from "../../../constants/constants";
 import { startHouseGame } from "../../houseGame/houseGame";
-import { soundOn } from "../soundButton/soundButton";
+import handleButtonClick from "../buttonFunction";
 
 export const handleHouseClick = () => {
-    const housesElement = document.getElementById("houses");
-    const housesButton = document.getElementById("houses-button") as HTMLButtonElement;
-    
-    if (housesElement && article) {
-        housesElement.addEventListener("click", async () => {
-            try {
-                if (drawingSwordAudio as HTMLAudioElement) {
-                    playSound(soundOn, drawingSwordAudio);
-                }
-    
-                clearAndAddClickedClass(housesButton);
-    
-                clearArticleAndAddBackground("url('./media/backgrounds/paper-mask-standing2.png')");
-                clearAside();
-
-                startHouseGame();
-    
-            } catch (error) {
-                console.error("Fel vid hämtning av hus:", error);
-            }
-        });
-    } else {
-        console.error("Element med ID 'houses' eller 'article' hittades inte.");
-    }
-}
+    handleButtonClick("houses", "houses-button", "./media/backgrounds/paper-mask-standing2.png", startHouseGame, drawingSwordAudio);
+};
