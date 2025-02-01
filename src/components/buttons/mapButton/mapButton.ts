7363b3c30b674fc9daf07e580becfbc83cd531a2
jessicaagren <jessicaagren@hotmail.com>
2025-02-01 15:17:51 +0100

    Gjort map till inforuta

--- src/components/buttons/mapButton/mapButton.ts
+++ src/components/buttons/mapButton/mapButton.ts
@@ -1,9 +1,10 @@
 import { flipPageAudio } from "../../../constants/constants";
+import setupInfo from "../../mapFunction/mapFunction";
 import handleButtonClick from "../handleButtonClick/handleButtonClick";
 
 
 const handleMapClick = () => {
-    handleButtonClick("map", "map-button", './media/backgrounds/distressed-map.png', undefined, flipPageAudio)
+    handleButtonClick("map", "map-button", './media/backgrounds/distressed-map.png', setupInfo, flipPageAudio)
 }
 
 export default handleMapClick;
---
+++ src/components/mapFunction/mapFunction.ts
@@ -0,0 +1,22 @@
+import { aside } from "../../constants/constants";
+
+const setupInfo = () => {
+    const infoContainer = document.createElement("div");
+    infoContainer.className = "containers";
+    infoContainer.id = "info-container";
+    aside.appendChild(infoContainer);
+
+    infoContainer.innerHTML = `
+        <h2 id="title">Scroll of Thrones</h2>
+        <section>
+        <h3>Made by: Jessica Ågren</h3>
+        <h4>API:s used:</h4>
+            <p>An API of Ice and Fire</p>
+            <p>Game of Thrones Quotes API</p>
+            <h4>House sigils from:</h4>
+            <p>Westeros</p>
+        </section>
+    `;
+};
+
+export default setupInfo;
