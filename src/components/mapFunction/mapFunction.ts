1f7e106778c8659d6e853b92c4ca5813fcc064c2
jessicaagren <jessicaagren@hotmail.com>
2025-02-03 11:14:25 +0100

    Lagt till länkar på credits

--- src/components/mapFunction/mapFunction.ts
+++ src/components/mapFunction/mapFunction.ts
@@ -1,20 +1,22 @@
 import { article } from "../../constants/constants";
 
 const setupInfo = () => {
-    const infoContainer = document.createElement("div");
-    infoContainer.className = "containers";
-    infoContainer.id = "info-container";
-    article.appendChild(infoContainer);
+    const creditsContainer = document.createElement("div");
+    creditsContainer.className = "containers";
+    creditsContainer.id = "credits-container";
+    article.appendChild(creditsContainer);
 
-    infoContainer.innerHTML = `
+    creditsContainer.innerHTML = `
         <h2 id="title">Scroll of Thrones</h2>
         <section>
         <h3>Made by: Jessica Ågren</h3>
+        <p><a href="https://www.linkedin.com/in/jessicaagren/">Linkedin</a></p>
+        <p><a href="https://github.com/jessicaagren">Github</a></p>
         <h4>API:s used:</h4>
-            <p>An API of Ice and Fire</p>
-            <p>Game of Thrones Quotes API</p>
+            <p><a href="https://anapioficeandfire.com/">An API of Ice and Fire</a></p>
+            <p><a href="https://gameofthronesquotes.xyz/">Game of Thrones Quotes API</a></p>
             <h4>House sigils from:</h4>
-            <p>Westeros</p>
+            <p><a href="https://awoiaf.westeros.org/index.php/List_of_Houses">A Wiki of Ice and Fire</a></p>
         </section>
     `;
 };
