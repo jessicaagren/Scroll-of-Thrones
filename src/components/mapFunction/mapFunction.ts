8c7313d1002cf5387c2c1c59d02db236abbe0331
jessicaagren <jessicaagren@hotmail.com>
2025-02-03 17:48:51 +0100

    Gjort så att länkar öppnas i ny tabb

--- src/components/mapFunction/mapFunction.ts
+++ src/components/mapFunction/mapFunction.ts
@@ -10,13 +10,13 @@ const setupInfo = () => {
         <h2 id="title">Scroll of Thrones</h2>
         <section>
         <h3>Made by: Jessica Ågren</h3>
-        <a href="https://www.linkedin.com/in/jessicaagren/"><img src="/media/icons/linkedin.png" alt="Linkedin" style="width:42px;height:42px;"></a>
-        <a href="https://github.com/jessicaagren"><img src="/media/icons/github.png" alt="Github" style="width:42px;height:42px;"></a>
+        <a href="https://www.linkedin.com/in/jessicaagren/" target="_blank"><img src="/media/icons/linkedin.png" alt="Linkedin" style="width:42px;height:42px;"></a>
+        <a href="https://github.com/jessicaagren" target="_blank"><img src="/media/icons/github.png" alt="Github" style="width:42px;height:42px;"></a>
         <h4>API:s used:</h4>
-            <p><a href="https://anapioficeandfire.com/">An API of Ice and Fire</a></p>
-            <p><a href="https://gameofthronesquotes.xyz/">Game of Thrones Quotes API</a></p>
+            <p><a href="https://anapioficeandfire.com/" target="_blank">An API of Ice and Fire</a></p>
+            <p><a href="https://gameofthronesquotes.xyz/" target="_blank">Game of Thrones Quotes API</a></p>
             <h4>House sigils from:</h4>
-            <p><a href="https://awoiaf.westeros.org/index.php/List_of_Houses">A Wiki of Ice and Fire</a></p>
+            <p><a href="https://awoiaf.westeros.org/index.php/List_of_Houses" target="_blank">A Wiki of Ice and Fire</a></p>
         </section>
     `;
 };
